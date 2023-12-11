9d45059f66a3e23f26f1e802dff95f52e7de8a32
rahin100 <rahinislam01@gmail.com>
2023-12-11 17:39:47 +0600

    setup all things

---
+++ src/Layouts/MainLayouts.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
+
+
+const MainLayouts = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default MainLayouts;
